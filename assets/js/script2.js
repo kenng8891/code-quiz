a35fda50d65383f9db4097f5835069820b3c1f57
Kenng8891 <ng.kennyy@gmail.com>
2022-05-15 10:50:45 -0400

    revise/edit

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,66 +1,60 @@
-var questions = ["Commonly used data types DO NOT inculde","Commonly used data types DO NOT inculde","String values must be enclosed within ____ when being assigned to variables","A very useful tool used during development and debugging for printing content to the debugger is:",]
-
-var question1 = [ "strings", "booleans", "alerts", "numbers"];
-var question2 = ["numbers and strings", "other arrays", "booleans", "all of the above"];
-var question3 = [ "commas", "curly brackets", "quotes", "parentheses"];
-var question4 = [ "Javascript", "terminal/bash", "for loops", "console.log"];
-var answers =  ["strings", "other arrays", "quotes", "parentheses"]
-
-var question11 = {
+var question1 = {
   q: "Commonly used data types DO NOT inculde",
   answers: [ "strings", "booleans", "alerts", "numbers"],
   correct: "strings"
 }
 
-var question22 = {
+var question2 = {
+  q: "The condition in an if/else statement is enclosed with:",
+  answers: ["quotes", "curly brackets", "parentheses", "square brackets"],
+  correct: "curly brackets"
+}
+var question3 = {
   q: "String values must be enclosed within ____ when being assigned to variables",
   answers: [ "commas", "curly brackets", "quotes", "parentheses"],
   correct: "parentheses",
 }
 
-var question44 = {
+var question4 = {
   q: "A very useful tool used during development and debugging for printing content to the debugger is:",
   answers: [ "Javascript", "terminal/bash", "for loops", "console.log"],
   correct: "console.log",
 }
 
+var questionCounter = 0;
+var selections = [];
 var count = 75;
+
 var timer = setInterval(function() {
   console.log(count);
   count--;
-    var question = document.getElementById('question-text');
-    question.textContent = question11.q;
-    var answer1 = document.getElementById('0');
-    answer1.textContent = question11.answers[0];
-    var answer2 = document.getElementById('1')
-    answer2.textContent = question11.answers[1];
-    var answer3 = document.getElementById('2')
-    answer3.textContent = question11.answers[2];
-    var answer4 = document.getElementById('3')
-    answer4.textContent = question11.answers[3];
-  
-  console.log(answer1);
   if(count === 0) {
     stopInterval()
   }
   document.getElementById("countdown-timer").innerHTML = count
-  populate();
 }, 1000);
 
 
 var stopInterval = function() {
-  console.log('time is up!');
   clearInterval(timer);
 }
-
-
-
 
 setInterval();
 stopInterval();
 
-console.log(questions[0], question1, answers[0])
-
+function createButtons(index) {
+  var buttonList = $('<ul>');
+  var item;
+  var input= '';
+  for (var i = 0; i < questions[index].answers.length; i++) {
+    item = $('<li>');
+    input = '<input type ="button" name = "answer" value= ' + i + ' />';
+    input += questions[index].answers[i];
+    item.append(input);
+    buttonList.append(item);
+  }
+  return buttonList
+}
 // create the for loop to loop through questions
 // we want to generate each questions and possible answers inside the forloop
 // target the elements with the id's and change the text content to the questions and answers
---
+++ assets/js/script4.js
@@ -0,0 +1,164 @@
+(function() {
+    var questions = [{
+      question: "Commonly used data types DO NOT inculde",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      correctAnswer: "strings"
+    }, {
+      question: "The condition in an if/else statement is enclosed with:",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      correctAnswer: "curly brackets"
+    }, {
+      question: "String values must be enclosed within ____ when being assigned to variables",
+      choices: [ "commas", "curly brackets", "quotes", "parentheses"],
+      correctAnswer: "parentheses",
+    }, {
+      question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: [ "Javascript", "terminal/bash", "for loops", "console.log"],
+      correct: "console.log",
+    }, 
+  ]
+    var questionCounter = 0; //Tracks question number
+    var selections = []; //Array containing user choices
+    var quiz = $('#quiz'); //Quiz div object
+    
+    // Display initial question
+    displayNext();
+    
+    // Click handler for the 'next' button
+    $('#next').on('click', function (e) {
+      e.preventDefault();
+      
+      // Suspend click listener during fade animation
+      if(quiz.is(':animated')) {        
+        return false;
+      }
+      choose();
+      
+      // If no user selection, progress is stopped
+      if (isNaN(selections[questionCounter])) {
+        alert('Please make a selection!');
+      } else {
+        questionCounter++;
+        displayNext();
+      }
+    });
+    
+    // Click handler for the 'prev' button
+    $('#prev').on('click', function (e) {
+      e.preventDefault();
+      
+      if(quiz.is(':animated')) {
+        return false;
+      }
+      choose();
+      questionCounter--;
+      displayNext();
+    });
+    
+    // Click handler for the 'Start Over' button
+    $('#start').on('click', function (e) {
+      e.preventDefault();
+      
+      if(quiz.is(':animated')) {
+        return false;
+      }
+      questionCounter = 0;
+      selections = [];
+      displayNext();
+      $('#start').hide();
+    });
+    
+    // Animates buttons on hover
+    $('.button').on('mouseenter', function () {
+      $(this).addClass('active');
+    });
+    $('.button').on('mouseleave', function () {
+      $(this).removeClass('active');
+    });
+    
+    // Creates and returns the div that contains the questions and 
+    // the answer selections
+    function createQuestionElement(index) {
+      var qElement = $('<div>', {
+        id: 'question'
+      });
+      
+      var header = $('<h2>Question ' + (index + 1) + ':</h2>');
+      qElement.append(header);
+      
+      var question = $('<p>').append(questions[index].question);
+      qElement.append(question);
+      
+      var radioButtons = createRadios(index);
+      qElement.append(radioButtons);
+      
+      return qElement;
+    }
+    
+    // Creates a list of the answer choices as radio inputs
+    function createRadios(index) {
+      var buttonList = $('<ul>');
+      var item;
+      var input = '';
+      for (var i = 0; i < questions[index].choices.length; i++) {
+        item = $('<li>');
+        input = '<input type="button" name="answer" value=' + questions[i].choices[i] + ' />';
+        input += questions[index].choices[i];
+        item.append(input);
+        buttonList.append(item);
+      }
+      return buttonList;
+    }
+    
+    // Reads the user selection and pushes the value to an array
+    function choose() {
+      selections[questionCounter] = +$('input[name="answer"]:checked').val();
+      console.log(selections)
+    }
+    
+    // Displays next requested element
+    function displayNext() {
+      quiz.fadeOut(function() {
+        $('#question').remove();
+        
+        if(questionCounter < questions.length){
+          var nextQuestion = createQuestionElement(questionCounter);
+          quiz.append(nextQuestion).fadeIn();
+          if (!(isNaN(selections[questionCounter]))) {
+            $('input[value='+selections[questionCounter]+']').prop('checked', true);
+          }
+          
+          // Controls display of 'prev' button
+          if(questionCounter === 1){
+            $('#prev').show();
+          } else if(questionCounter === 0){
+            
+            $('#prev').hide();
+            $('#next').show();
+          }
+        }else {
+          var scoreElem = displayScore();
+          quiz.append(scoreElem).fadeIn();
+          $('#next').hide();
+          $('#prev').hide();
+          $('#start').show();
+        }
+      });
+    }
+    
+    // Computes score and returns a paragraph element to be displayed
+    function displayScore() {
+      var score = $('<p>',{id: 'question'});
+      
+      var numCorrect = 0;
+      for (var i = 0; i < selections.length; i++) {
+        if (selections[i] === questions[i].correctAnswer) {
+          numCorrect++;
+        }
+      }
+      
+      score.append('You got ' + numCorrect + ' questions out of ' +
+                   questions.length + ' right!!!');
+      return score;
+    }
+  })();
